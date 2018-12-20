d535dc6ec60e2d6b5dce9c6fb3549c98851dc076
Bill Murphy <bill559m@gmail.com>
2018-12-20 12:14:07 -0800

    express server running & webpack setup & compiling React code to a bundle

---
+++ client/src/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// import Board from './components/Board.jsx';
+
+ReactDOM.render("Hello from React", document.getElementById("app"));
+
+
+
