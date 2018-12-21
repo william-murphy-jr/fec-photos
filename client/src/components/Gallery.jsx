b3ad62b84a8a6f536eb35cf8ba04e99765651d19
Bill Murphy <bill559m@gmail.com>
2018-12-21 15:27:11 -0800

    1 x 2 x 2 total of 5 images now on page rough formating

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React from "react";
 
 class Gallery extends React.Component {
   constructor(props) {
     super(props);
-
   }
 
-  render() { 
-    return  (
-      <div className="container-fluid" style={{marginLeft: "-15px"}}>
+  render() {
+    return (
+      <div className="container-fluid" style={{ marginLeft: "-15px" }}>
         <div className="row">
-          <div className="col-6"><img src="http://lorempixel.com/800/225" /></div>
-          <div className="col-6"><img src="http://lorempixel.com/800/225" /></div>
+          <div className="col-6">
+            <img class="img-fluid" src="http://lorempixel.com/800/225" />
+          </div>
+          <div className="col-6">
+            <div className="row">
+              <div className="col-6">
+                <img class="img-fluid" src="http://lorempixel.com/800/225" />
+              </div>
+              <div className="col-6">
+                <img class="img-fluid" src="http://lorempixel.com/800/225" />
+              </div>
+              <div className="col-6">
+                <img class="img-fluid" src="http://lorempixel.com/800/225" />
+              </div>
+              <div className="col-6">
+                <img class="img-fluid" src="http://lorempixel.com/800/225" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
-
   }
 }
 export default Gallery;
