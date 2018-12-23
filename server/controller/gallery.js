6192eb9c2b2dc572a063a200ff37b5c9167a959b
Bill Murphy <bill559m@gmail.com>
2018-12-23 10:29:21 -0800

    changed db query to return first a primary image then the secondary images

--- server/controller/gallery.js
+++ server/controller/gallery.js
@@ -1,12 +1,23 @@
 const db = require('../database/db');
 
+const result = [];
+
 const getGalleryImages = function(req, res) {
   db.DISPLAY_IMAGE.findAll({
-    where: { primaryImage: false },
-    limit: 5
+    where: { primaryImage: true },
+    limit: 1
   })
     .then(function(data) {
-      res.json(data);
+      result.concat(data);
+      db.DISPLAY_IMAGE.findAll({
+        where: {
+          primaryImage: false
+        },
+        limit: 10
+      }).then(function(data2) {
+        res.json(data.concat(data2));
+      });
+
       // console.log("******** DATA *******", data);
     });
 };
