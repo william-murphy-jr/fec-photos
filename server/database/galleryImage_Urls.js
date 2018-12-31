b2ac4457b15a268483f672ade6a515067d7111b1
Bill Murphy <bill559m@gmail.com>
2018-12-31 14:56:08 -0800

    working with 1200x650 images

--- server/database/galleryImage_Urls.js
+++ server/database/galleryImage_Urls.js
@@ -1,42 +1,20 @@
 const front = [
-  'https://s3-us-west-1.amazonaws.com/image-store-100/front-01-house-2469067_960_720.jpg',
+  'https://s3-us-west-1.amazonaws.com/image-store-100/front-07-BH+Cali+Pond+Ext1200x650.jpg',
+  'https://s3-us-west-1.amazonaws.com/image-store-100/front-05-16547490-151101.jpg',
   'https://s3-us-west-1.amazonaws.com/image-store-100/front-02-sea-01_i21161920x0_7_36.jpg',
+  'https://s3-us-west-1.amazonaws.com/image-store-100/front-01-house-2469067_960_720.jpg',
   'https://s3-us-west-1.amazonaws.com/image-store-100/front-03-Buckingham-Exterior_1200x650.jpg',
   'https://s3-us-west-1.amazonaws.com/image-store-100/front-04-19965_eleM_lg.jpg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/front-05-16547490-151101.jpg',
   'https://s3-us-west-1.amazonaws.com/image-store-100/front-06-Sierra-TH-19_PLEX.jpeg'
 
 ];
 
 const interior = [
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-001-pexels-photo-276724.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-002-pexels-photo-1571460.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-003-pexels-photo-259962.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-004-living-room-couch-interior-room-584399.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-005-pexels-photo-279719.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-001-pexels-photo-276724.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-001-pexels-photo-276724.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-001-pexels-photo-276724.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-002-pexels-photo-1571460.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-003-pexels-photo-259962.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-004-living-room-couch-interior-room-584399.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-005-pexels-photo-279719.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-006-kitchen-stove-sink-kitchen-counter-349749.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-007-pexels-photo-279719.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-008-pexels-photo-1282315.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-009-pexels-photo-1082355.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-010-pexels-photo-709767.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-011-pexels-photo-279607.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-012-pexels-photo-534151.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-013-pexels-photo-271624.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-014-pexels-photo-1027512.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-015-pexels-photo-271816.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-015-pexels-photo-276554.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-016-pexels-photo-259580.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-017-pexels-photo-257344.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-018-pexels-photo-261045.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-019-pexels-photo-1571463.jpeg',
-  'https://s3-us-west-1.amazonaws.com/image-store-100/Interior-020-pexels-photo-271795.jpeg'
+  'https://s3-us-west-1.amazonaws.com/image-store-100/interior-001-Port-Phillip-11-1200x650.jpg',
+  'https://s3-us-west-1.amazonaws.com/image-store-100/interior-002-Somerton+1200x650.jpg',
+  'https://s3-us-west-1.amazonaws.com/image-store-100/interior-003-Shoreline-9-1200x650.jpg',
+  'https://s3-us-west-1.amazonaws.com/image-store-100/interior-004-Elysium-7-1200x650.jpg',
+  'https://s3-us-west-1.amazonaws.com/image-store-100/interior-005-Second-5-1200x650.jpg'
 ];
 
 exports.front = front;
