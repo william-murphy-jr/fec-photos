e3636b8ff2e38e92919b9737ebed4c4a6b0b172a
Bill Murphy <bill559m@gmail.com>
2018-12-30 18:43:41 -0800

    checking css & images for S3

--- client/src/components/GalleryQuarter.jsx
+++ client/src/components/GalleryQuarter.jsx
@@ -3,18 +3,14 @@ import React from 'react';
 const GalleryQuarter = function(props) {
   // console.log('Quarter -- props.highLight: ', props.highLight);
 
-  return (
-    <div className={'col-6 quarter' + ' pos' + props.position}>
-      <img
-        className={props.highLight ? 'img-fluid' : 'img-fluid overlay-img'}
-        data-position={props.position}
-        onMouseOver={props.hoverOn}
-        onMouseOut={props.hoverOff}
-        src={props.image}
-        onClick={props.handleImageClickShowCarousel}
-      />
-    </div>
-  );
+  return <div className={'col-6 quarter' + ' pos' + props.position}>
+    <img className={props.highLight ? 'img-fluid img-gallery-qtr' : 'img-fluid img-gallery-qtr overlay-img'}
+      data-position={props.position}
+      onMouseOver={props.hoverOn}
+      onMouseOut={props.hoverOff}
+      src={props.image}
+      onClick={props.handleImageClickShowCarousel} />
+    </div>;
 };
 
 export default GalleryQuarter;
