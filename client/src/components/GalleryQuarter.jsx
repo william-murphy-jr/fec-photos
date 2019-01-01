d9b6e325fe2f8dc0461dab57b557747cf33e2336
Bill Murphy <bill559m@gmail.com>
2019-01-01 14:54:48 -0800

    added ShareSaveBtns component rendering

--- client/src/components/GalleryQuarter.jsx
+++ client/src/components/GalleryQuarter.jsx
@@ -7,26 +7,12 @@ const GalleryQuarter = function(props) {
   if(props.position == '2') { 
     console.log('props.position: ', props.position); 
     return (
-      <div className={'col-6 quarter' + ' pos' + props.position}>
-        <div className='share-save'>
-          <button className="galleryQtrBtn">
-            <i className='far fa-share-square share'></i>
-            <span className="galleryQtrBtnTxt">
-              <span className='innerGalleryQtrBtnTxt'>Share</span>
-            </span>
-          </button>
-          <button className="galleryQtrBtn">
-            <i className="far fa-heart save"></i>
-            <span className="galleryQtrBtnTxt">Save</span>
-          </button>
-        </div>
-      <img className={props.highLight ? 'img-fluid img-gallery-qtr' : 'img-fluid img-gallery-qtr overlay-img'} data-position={props.position} onMouseOver={props.hoverOn} onMouseOut={props.hoverOff} src={props.image} onClick={props.handleImageClickShowCarousel} />
-    </div>);
+      <ShareSaveBtns />
   } else {
-  return (
-    <div className={'col-6 quarter' + ' pos' + props.position}>
-      <img className={props.highLight ? 'img-fluid img-gallery-qtr' : 'img-fluid img-gallery-qtr overlay-img'} data-position={props.position} onMouseOver={props.hoverOn} onMouseOut={props.hoverOff} src={props.image} onClick={props.handleImageClickShowCarousel} />
-    </div>);
+    return (
+      <div className={'col-6 quarter' + ' pos' + props.position}>
+        <img className={props.highLight ? 'img-fluid img-gallery-qtr' : 'img-fluid img-gallery-qtr overlay-img'} data-position={props.position} onMouseOver={props.hoverOn} onMouseOut={props.hoverOff} src={props.image} onClick={props.handleImageClickShowCarousel} />
+      </div>);
   }
 
   // }
--- client/src/components/ShareSaveBtns.jsx
+++ client/src/components/ShareSaveBtns.jsx
@@ -15,7 +15,9 @@ const ShareSaveBtns = function(props) {
           <span className="galleryQtrBtnTxt">Save</span>
         </button>
       </div>
-    <img className={props.highLight ? 'img-fluid img-gallery-qtr' : 'img-fluid img-gallery-qtr overlay-img'} data-position={props.position} onMouseOver={props.hoverOn} onMouseOut={props.hoverOff} src={props.image} onClick={props.handleImageClickShowCarousel} />
+      <img className={props.highLight ? 'img-fluid img-gallery-qtr' : 'img-fluid img-gallery-qtr overlay-img'} data-position={props.position} onMouseOver={props.hoverOn} onMouseOut={props.hoverOff} src={props.image} onClick={props.handleImageClickShowCarousel} />
     </div>
   );
-}
+};
+
+export default ShareSaveBtns;
