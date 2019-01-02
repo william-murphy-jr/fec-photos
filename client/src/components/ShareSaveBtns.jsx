c1ca3da9988df515a01c2940ea8623e3971053dd
Bill Murphy <bill559m@gmail.com>
2019-01-01 17:43:29 -0800

    added hover to button

--- client/src/components/ShareSaveBtns.jsx
+++ client/src/components/ShareSaveBtns.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 const ShareSaveBtns = function(props) {
   return (
-      <div className='share-save'>
-        <button className="galleryQtrBtn">
-          <i className='far fa-share-square share'></i>
-          <span className="galleryQtrBtnTxt">
-            <span className='innerGalleryQtrBtnTxt'>Share</span>
-          </span>
-        </button>
-        <button className="galleryQtrBtn">
-          <i className="far fa-heart save"></i>
-          <span className="galleryQtrBtnTxt">Save</span>
-        </button>
-      </div>
+    <div className='share-save'>
+      <button className="galleryQtrBtn">
+        <i className='far fa-share-square share'></i>
+        <span className="galleryQtrBtnTxt">
+          <span className='innerGalleryQtrBtnTxt'>Share</span>
+        </span>
+      </button>
+      <button className="galleryQtrBtn">
+        <i className="far fa-heart save"></i>
+        <span className="galleryQtrBtnTxt">Save</span>
+      </button>
+    </div>
   );
 };
 
