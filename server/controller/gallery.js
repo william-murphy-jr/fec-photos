6f4bbc9c6d7ed442312f160b982d1f4ba0d80ffb
Bill Murphy <bill559m@gmail.com>
2019-01-03 21:25:50 -0800

    added random interior selection

--- server/controller/gallery.js
+++ server/controller/gallery.js
@@ -3,9 +3,11 @@ const db = require('../database/db');
 const result = [];
 
 const getGalleryImages = function(req, res) {
+  const numOfInteriorImages = 6;
+
   db.DISPLAY_IMAGE.findAll({
     where: { primaryImage: true },
-    limit: 10
+    // limit: 10
   })
     .then(function(data) {
       result.concat(data);
@@ -13,12 +15,22 @@ const getGalleryImages = function(req, res) {
         where: {
           primaryImage: false
         },
-        limit: 6
+        // limit: 10
       })
         .then(function(data2) {
           const front = [];
+          const interior = [];
+          let copy = [];
+          copy = data2.slice();
+
+          for (let i = 0; i < numOfInteriorImages; i++) {
+            let num = Math.floor(Math.random() * copy.length);
+            interior.push(copy[num]);
+            copy.splice(num, 1);
+          }
+
           front.push(data[Math.floor(Math.random() * data.length)]);
-          res.json(front.concat(data2));
+          res.json(front.concat(interior));
         });
     });
 };
