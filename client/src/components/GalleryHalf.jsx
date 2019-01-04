0f6c2f58a9808e3a66ac2ca70ff99bb52fa90646
Bill Murphy <bill559m@gmail.com>
2019-01-03 20:15:52 -0800

    raised carousel z-index to 999 and added galleryHalf img property

--- client/src/components/GalleryHalf.jsx
+++ client/src/components/GalleryHalf.jsx
@@ -5,7 +5,7 @@ const GalleryHalf = function (props) {
   return (
     <div className="col-6 half" data-position={props.position}>
       <img
-        className={props.highLight ? 'img-fluid' : 'img-fluid overlay-img'}
+        className={props.highLight ? 'img-fluid img-main' : 'img-fluid overlay-img img-main'}
         data-position={props.position}
         onMouseOver={props.hoverOn}
         onMouseOut={props.hoverOff}
